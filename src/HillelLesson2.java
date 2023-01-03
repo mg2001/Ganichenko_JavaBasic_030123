37c53d89ad5269e0be344919d05961e48093f302
Maxim Ganichenko <maximganichenko@MacBook-MG.local>
2023-01-03 15:34:23 +0200

    my first commit

---
+++ src/HillelLesson2.java
@@ -0,0 +1,9 @@
+public class HillelLesson2 {
+    public static void main(String [] args){
+        double latitude = 46.449347;
+        double longitude = 30.742126;
+        System.out.println("LAT-> "+latitude);
+        System.out.println("LON-> "+longitude);
+
+    }
+}
